405fe4a915fe12a6148fed0ebf6f11d77ec346d4
David Park <david@watermelonfueled.com>
2015-09-04 20:44:09 -0400

    added relative positioning table

--- javascript/setup.js
+++ javascript/setup.js
@@ -23,70 +23,73 @@ function main(){
 		addScreen(screenSettings,i);
 	}
 	setup.insertBefore(screenCountNode, screenSettings);
+}
 
-	function addScreen(screenSettings,count){
-		var newScreen = document.createElement("div");
-		newScreen.setAttribute("class","screen");
-		var screenId = "screen" + count;
-		newScreen.setAttribute("id",screenId);
-		newScreen.style.display="none";
+function addScreen(screenSettings,count){
+	var newScreen = document.createElement("div");
+	newScreen.setAttribute("class","screen");
+	var screenId = "screen" + count;
+	newScreen.setAttribute("id",screenId);
+	newScreen.style.display="none";
 
-		var relativePosition = relativePositionSetting();
-		newScreen.appendChild(relativePosition);
+	var relativePosition = relativePositionSetting();
+	newScreen.appendChild(relativePosition);
 
-		screenSettings.appendChild(newScreen);
+	screenSettings.appendChild(newScreen);
+}
+
+function relativePositionSetting() {
+	var boxAbove = document.createElement("td");
+	boxAbove.setAttribute("align","center");
+	boxAbove.appendChild(document.createTextNode("Above"));
+	var boxLeft = boxAbove.cloneNode(false);
+	boxLeft.appendChild(document.createTextNode("Left"));
+	var boxRight = boxAbove.cloneNode(false);
+	boxRight.appendChild(document.createTextNode("Right"));
+	var boxMain = boxAbove.cloneNode(false);
+	boxMain.appendChild(document.createTextNode("Main"));
+	boxAbove.setAttribute("colspan","3");
+	var boxBelow = boxAbove.cloneNode(false);
+	boxBelow.appendChild(document.createTextNode("Below"));
+
+	var row = document.createElement("tr");
+	var row2 = row.cloneNode(true);
+	var row3 = row.cloneNode(true);
+
+	row.appendChild(boxAbove);
+	row2.appendChild(boxLeft);
+	row2.appendChild(boxMain);
+	row2.appendChild(boxRight);
+	row3.appendChild(boxBelow);
+	
+	var table = document.createElement("table");
+	table.setAttribute("class","relativePosition");
+	table.appendChild(row);
+	table.appendChild(row2);
+	table.appendChild(row3);
+	
+	return table;
+}
+
+
+
+function buttonSet(button, num){
+	button.addEventListener("click", function(){screenSettingsDisplay(num);});
+}
+
+function screenSettingsDisplay(count) {
+	
+	var screenCountOld = screenSettings.getAttribute("data-screencount");
+	var screenList = screenSettings.getElementsByClassName("screen");
+	
+	//show more
+	for (var i = screenCountOld; i < count; i++) {
+		screenList[i].style.display="inline";
+	}
+	//show less
+	for (var i = screenCountOld; i > count; i--) {
+		screenList[i-1].style.display="none";
 	}
 
-	function relativePositionSetting() {
-		var boxAbove = document.createElement("td");
-		boxAbove.appendChild(document.createTextNode("Above"));
-		var boxBelow = boxAbove.cloneNode(false);
-		boxBelow.appendChild(document.createTextNode("Below"));
-		var boxLeft = boxAbove.cloneNode(false);
-		boxLeft.appendChild(document.createTextNode("Left"));
-		var boxRight = boxAbove.cloneNode(false);
-		boxRight.appendChild(document.createTextNode("Right"));
-		var boxMain = boxAbove.cloneNode(false);
-		boxMain.appendChild(document.createTextNode("Main"));
-		
-		var row = document.createElement("tr");
-		var row2 = row.cloneNode(true);
-		var row3 = row.cloneNode(true);
-
-		row.appendChild(boxAbove);
-		row2.appendChild(boxLeft);
-		row2.appendChild(boxMain);
-		row2.appendChild(boxRight);
-		row3.appendChild(boxBelow);
-		
-		var table = document.createElement("table");
-		table.appendChild(row);
-		table.appendChild(row2);
-		table.appendChild(row3);
-		
-		return table;
-	}
-
-
-
-	function buttonSet(button, num){
-		button.addEventListener("click", function(){screenSettingsDisplay(num);});
-	}
-
-	function screenSettingsDisplay(count) {
-		
-		var screenCountOld = screenSettings.getAttribute("data-screencount");
-		var screenList = screenSettings.getElementsByClassName("screen");
-		
-		//show more
-		for (var i = screenCountOld; i < count; i++) {
-			screenList[i].style.display="inline";
-		}
-		//show less
-		for (var i = screenCountOld; i > count; i--) {
-			screenList[i-1].style.display="none";
-		}
-
-		screenSettings.setAttribute("data-screencount", count);
-	}
+	screenSettings.setAttribute("data-screencount", count);
 }
